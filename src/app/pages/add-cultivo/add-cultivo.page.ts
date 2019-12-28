326399eb4b79d236d0261d53caf388044bd5e68d
Duvan sgf <damejia.98@hotmail.com>
2019-12-28 00:07:42 -0500

    Agregar Cultivo 1.0

---
+++ src/app/pages/add-cultivo/add-cultivo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-cultivo',
+  templateUrl: './add-cultivo.page.html',
+  styleUrls: ['./add-cultivo.page.scss'],
+})
+export class AddCultivoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
